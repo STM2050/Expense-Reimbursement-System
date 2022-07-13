6ac547d51d7e9e326447f7b0b7b9ad09d9b96a6c
Bipul Chaudhary <bipulchau@gmail.com>
2022-07-13 13:29:20 -0400

    format users_controller

--- controller/users_contoller.py
+++ controller/users_contoller.py
@@ -1,13 +1,15 @@
 import flask
 from flask import Blueprint, request
 
-uc = Blueprint("user_controller",__name__)
+uc = Blueprint("user_controller", __name__)
+
 
 @uc.route('/')
 @uc.route('/home')
 @uc.route('/index')
 def get_home_page():
     return "Welcome To Reimbursement App"
+
 
 @uc.route('/users/<user_id>')
 def get_user_reimbursement(user_id):
