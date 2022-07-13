bbcb78065a1b9fea289c062ddaf1b41373ccad62
STM2050 <shaquera.moore@gmail.com>
2022-07-13 12:57:51 -0400

    display reimbursement of a user

--- controller/users_contoller.py
+++ controller/users_contoller.py
@@ -8,3 +8,7 @@ uc = Blueprint("user_controller",__name__)
 @uc.route('/index')
 def get_home_page():
     return "Welcome To Reimbursement App"
+
+@uc.route('/users/<user_id>')
+def get_user_reimbursement(user_id):
+    return "At controller Layer"
